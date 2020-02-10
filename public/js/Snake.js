d741a971512f28e3027e2c46f3bc2973b0c15b39
esterlilianlb <esterlilian2231@gmail.com>
2020-02-10 17:54:59 -0300

    dying fase 2

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -88,5 +88,11 @@ export default class Snake {
             this.body[0].y >= this.scene.game.config.height) {
                 this.scene.scene.restart();
         }
+
+        //death by eating own tail
+        let tail = this.body.slice(1);
+        if(tail.filter(s => s.x === this.body[0].x && s.y === this.body[0].y).length > 0) {
+            this.scene.scene.restart();
+        }
     }
 }
