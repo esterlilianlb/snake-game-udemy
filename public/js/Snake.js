bc601e87b361604d8214e5231d7c79dc6a2488f9
esterlilianlb <esterlilian2231@gmail.com>
2020-02-10 13:55:10 -0300

    snake changing direction as we hit the arrow buttons

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -1,11 +1,38 @@
 export default class Snake {
     constructor(scene) {
         this.scene = scene;
+        this.direction = Phaser.Math.Vector2.RIGHT;
         this.body = [];
-        this.box = this.scene.add.rectangle(0, 0, 16, 16, 0xff0000).setOrigin(0);
+        this.body.push(
+            this.scene.add.rectangle(100, 100, 16, 16, 0xff0000).setOrigin(0)
+        );
+        this.body.push(
+            this.scene.add.rectangle(0, 0, 16, 16, 0x0000ff).setOrigin(0)
+        );
+        scene.input.keyboard.on('keydown', e => {
+            this.keydown(e)})
+    }
+
+    keydown(event) {
+        console.log(event);
+        switch(event.keyCode) {
+            case 37: //Left
+            this.direction = Phaser.Math.Vector2.LEFT;
+            break;
+            case 38: //Up
+            this.direction = Phaser.Math.Vector2.UP;
+            break;
+            case 39: //Right
+            this.direction = Phaser.Math.Vector2.RIGHT;
+            break;
+            case 40: //Down
+            this.direction = Phaser.Math.Vector2.DOWN;
+            break;
+        }
     }
 
     update(time) {
-        this.box.x += 1;
+        this.body[0].x += this.direction.x;
+        this.body[0].y += this.direction.y;
     }
 }
